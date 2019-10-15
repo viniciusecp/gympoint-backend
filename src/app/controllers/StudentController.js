12257bcba29fe47a0c7b5a99d153b3a4dc1cfaf0
Vinicius Silva <viniciusfs.ecp@gmail.com>
2019-10-15 17:02:45 -0300

    Initial commit

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,39 @@
+import * as Yup from 'yup';
+
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number()
+        .required()
+        .integer()
+        .positive(),
+      height: Yup.number()
+        .required()
+        .min(0),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists.' });
+    }
+
+    const { id, name, email, age, height } = await Student.create(req.body);
+
+    return res.json({ id, name, email, age, height });
+  }
+}
+
+export default new StudentController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '14525c5326961f34e2c70411850b4aa9',
+  expiresIn: '7d',
+};
